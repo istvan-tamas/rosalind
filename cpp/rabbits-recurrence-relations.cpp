cefcb249c9b4649eec06c7cf1105a5f9afc5eee4
istvan-tamas <istvan.tamas90@gmail.com>
2025-01-11 18:04:34 +0100

    Rabbits and Recurrence implementation

--- cpp/rabbits-recurrence-relations.cpp
+++ cpp/rabbits-recurrence-relations.cpp
@@ -1,5 +1,21 @@
-#include <iostream>;
+#include <iostream>
+
+int recurrence(int n, int k){
+
+    int result = 1;
+
+    for(int i = 0; i <= n; i++){
+        result = result + k;
+    }
+
+    return result;
+}
+
 
 int main(){
 
+    int n = 29;
+    int k = 5;
+
+    std::cout << recurrence(n, k);
 };
